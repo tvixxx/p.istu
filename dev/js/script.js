51b5a7415f4e0a2a8792e87e78233ea0c8faefbc
shka <shka@htc-cs.ru>
2017-05-17 00:32:43 +0400

    up

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,27 +1,90 @@
-
-(function(window) {
-
-    let html = $('html');
-    let htmlHeight = html.height();
-
-    // html.css({'height': htmlHeight + 'px'});
-
-    // $('.js-primary-content-scroll').mCustomScrollbar();
-    // $('.js-secondary-content-scroll').mCustomScrollbar();
-
-    // $('.js-primary-content-scroll').scrollbar();
-
-
-    // (function($){
-    //     $(window).on("load",function() {
-    //         $('.primary-content__body').mCustomScrollbar({
-    //             theme: "light",
-    //         });
-    //     });
-    // })(jQuery);
-})(window);
+//
+// (function(window) {
+//
+//     let html = $('html');
+//     let htmlHeight = html.height();
+//
+//     // html.css({'height': htmlHeight + 'px'});
+//
+//     // $('.js-primary-content-scroll').mCustomScrollbar();
+//     // $('.js-secondary-content-scroll').mCustomScrollbar();
+//
+//     // $('.js-primary-content-scroll').scrollbar();
+//
+//
+//     // (function($){
+//     //     $(window).on("load",function() {
+//     //         $('.primary-content__body').mCustomScrollbar({
+//     //             theme: "light",
+//     //         });
+//     //     });
+//     // })(jQuery);
+// })(window);
+//
+// $(document).ready(function(){
+//     // $('.articles-module').perfectScrollbar();
+//     // $('.articles-module__list').mCustomScrollbar();
+// });
 
 $(document).ready(function(){
-    // $('.articles-module').perfectScrollbar();
-    // $('.articles-module__list').mCustomScrollbar();
+
+    // var html = $('html');
+    //
+    // var htmlHeight = html.height();
+    // var navMenuHeight = $('.secondary-nav-menu').height();
+    // var htmlHeightForWrapperFromContainer = htmlHeight;
+    //
+    // var $primaryContentContainer = $('#content1');
+    // var $content = $primaryContentContainer.find('.primary-content__content');
+    //
+    // $('.list-content__section--row').height($('.list-content__section--row').height() - 300);
+    //
+    // // var $parent = $primaryContent.parent();
+    // // var parentHeight = $parent.height();
+    //
+    // var primaryContentHeight = $primaryContentContainer.height(htmlHeightForWrapperFromContainer);
+    //
+    // $primaryContentContainer.perfectScrollbar();
+    // $('.primary-content__body').perfectScrollbar();
+    // $('.js-secondary-content-scroll').perfectScrollbar();
+    // $('.js-primary-content-scroll').mCustomScrollbar();
+
+    // $('.js-primary-content-scroll').nicescroll();
+
+    $(window).on("load",function(){
+
+        function debounce(func, wait, immediate) {
+            var timeout;
+            return function() {
+                var context = this, args = arguments;
+                var later = function() {
+                    timeout = null;
+                    if (!immediate) func.apply(context, args);
+                };
+                var callNow = immediate && !timeout;
+                clearTimeout(timeout);
+                timeout = setTimeout(later, wait);
+                if (callNow) func.apply(context, args);
+            };
+        }
+
+        var $primaryContentScroll = $('.js-primary-content-scroll');
+        $primaryContentScroll.height($('html').innerHeight() - 80);
+        $primaryContentScroll.mCustomScrollbar({ advanced:{updateOnContentResize:true} });
+
+        var $secondaryContentScroll =  $('.js-secondary-content-scroll');
+        $secondaryContentScroll.height($('html').height() - 80);
+        $secondaryContentScroll.mCustomScrollbar({ advanced:{updateOnContentResize:true} });
+
+
+        $(window).on('resize', debounce(function () {
+            $('html').height($('html').height());
+
+            $primaryContentScroll.height($('html').height() - 80);
+            $secondaryContentScroll.height($('html').height() - 80);
+
+            $primaryContentScroll.mCustomScrollbar('update');
+        }, 200));
+    });
 });
+
