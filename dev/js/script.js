fe2559fb8a46f656ee6befc22e6b00568902a835
kirillshilov <kirill.shilov@piano.io>
2017-05-16 19:35:45 +0400

    updated structure and design

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,4 +1,27 @@
-(function(global){
 
-    console.log('hey!');
-}(window));
+(function(window) {
+
+    let html = $('html');
+    let htmlHeight = html.height();
+
+    // html.css({'height': htmlHeight + 'px'});
+
+    // $('.js-primary-content-scroll').mCustomScrollbar();
+    // $('.js-secondary-content-scroll').mCustomScrollbar();
+
+    // $('.js-primary-content-scroll').scrollbar();
+
+
+    // (function($){
+    //     $(window).on("load",function() {
+    //         $('.primary-content__body').mCustomScrollbar({
+    //             theme: "light",
+    //         });
+    //     });
+    // })(jQuery);
+})(window);
+
+$(document).ready(function(){
+    // $('.articles-module').perfectScrollbar();
+    // $('.articles-module__list').mCustomScrollbar();
+});
