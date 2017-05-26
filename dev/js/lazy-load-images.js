51757016b2bb85d8d469f3d0faa2b87df9756f41
shka <shka@htc-cs.ru>
2017-05-26 09:30:43 +0400

    updated styles for lazy-load

---
+++ dev/js/lazy-load-images.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    var echo_class = 'echo';
+    var echo_hidden = 'echo-hidden';
+    var echo_loaded = 'echo-loaded';
+
+    echo.init({
+        offset: 100,
+        throttle: 250,
+        unload: false,
+        callback: function (element, op) {
+
+            $(element).addClass('' + echo_class + ' ' + echo_hidden);
+
+            setTimeout(function () {
+                $(element)
+                    .removeClass(echo_hidden)
+                    .addClass(echo_loaded);
+            }, 250);
+
+            console.log(element, 'has been', op + 'ed');
+        }
+    });
+
+    $(window).on('load resize', function(){
+        echo.render();
+    });
+});
+
+
+
