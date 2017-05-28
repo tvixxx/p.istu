b7ce95c0dc9d6b7671b1dcf30fdecc33a3d3e81e
shka <shka@htc-cs.ru>
2017-05-28 09:45:18 +0400

    updated js, lazy load , added program card

--- dev/js/lazy-load-images.js
+++ dev/js/lazy-load-images.js
@@ -1,29 +1,38 @@
 $(document).ready(function(){
-    var echo_class = 'echo';
-    var echo_hidden = 'echo-hidden';
-    var echo_loaded = 'echo-loaded';
+    // var echo_class = 'echo';
+    // var echo_hidden = 'echo-hidden';
+    // var echo_loaded = 'echo-loaded';
+    //
+    // echo.init({
+    //     offset: 100,
+    //     throttle: 250,
+    //     unload: false,
+    //     callback: function (element, op) {
+    //
+    //         $(element).addClass('' + echo_class + ' ' + echo_hidden);
+    //
+    //         setTimeout(function () {
+    //             $(element)
+    //                 .removeClass(echo_hidden)
+    //                 .addClass(echo_loaded);
+    //         }, 250);
+    //
+    //         console.log(element, 'has been', op + 'ed');
+    //     }
+    // });
+    //
+    // $(window).on('load resize', function(){
+    //     echo.render();
+    // });
 
-    echo.init({
-        offset: 100,
-        throttle: 250,
-        unload: false,
-        callback: function (element, op) {
-
-            $(element).addClass('' + echo_class + ' ' + echo_hidden);
-
-            setTimeout(function () {
-                $(element)
-                    .removeClass(echo_hidden)
-                    .addClass(echo_loaded);
-            }, 250);
-
-            console.log(element, 'has been', op + 'ed');
-        }
-    });
-
-    $(window).on('load resize', function(){
-        echo.render();
-    });
+    // $(function() {
+    //     $("img.lazy").lazyload({
+    //         threshold : 200,
+    //         effect : "fadeIn",
+    //         // container: $(".post--news")
+    //         container: $(".js-primary-content-body")
+    //     });
+    // });
 });
 
 
